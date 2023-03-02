caa81b5cc98929ab7280769922e4fb428ec76715
1night2zero <1night2zero@gmail.com>
2023-03-02 16:41:14 +0800

    特定、自定义异常处理

--- common/service_base/src/main/java/zstu/edu/servicebase/exceptionhandler/GlobalExceptionHandler.java
+++ common/service_base/src/main/java/zstu/edu/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -14,4 +14,21 @@ public class GlobalExceptionHandler {
         e.printStackTrace();
         return R.error().message("执行了全局异常处理...");
     }
+
+    // 特定异常处理 / by zero
+    @ExceptionHandler(ArithmeticException.class)
+    @ResponseBody
+    public R error(ArithmeticException e) {
+        e.printStackTrace();
+        return R.error().message("执行了ArithmeticException异常处理...");
+    }
+
+    // 自定义异常处理
+    @ExceptionHandler(MyException.class)
+    @ResponseBody
+    public R error(MyException e) {
+        e.printStackTrace();
+        return R.error().code(e.getCode()).message(e.getMsg());
+    }
+
 }
--- service/service_edu/src/main/java/zstu/edu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/zstu/edu/eduservice/controller/EduTeacherController.java
@@ -14,6 +14,7 @@ import zstu.edu.commonutils.R;
 import zstu.edu.eduservice.entity.EduTeacher;
 import zstu.edu.eduservice.entity.vo.TeacherQuery;
 import zstu.edu.eduservice.service.EduTeacherService;
+import zstu.edu.servicebase.exceptionhandler.MyException;
 
 import java.util.List;
 
@@ -44,7 +45,6 @@ public class EduTeacherController {
     @GetMapping("findAll")
     public R findAllTeacher() {
         List<EduTeacher> list = teacherService.list(null);
-
         return R.ok().data("items", list);
     }
 
