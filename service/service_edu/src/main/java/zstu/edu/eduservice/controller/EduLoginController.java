2a7cb65ae41561e176448d9713802737f4acb43e
1night2zero <1night2zero@gmail.com>
2023-03-03 18:26:37 +0800

    后台登录接口

---
+++ service/service_edu/src/main/java/zstu/edu/eduservice/controller/EduLoginController.java
@@ -0,0 +1,21 @@
+package zstu.edu.eduservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import zstu.edu.commonutils.R;
+
+@RestController
+@RequestMapping("/eduservice/user")
+public class EduLoginController {
+    @PostMapping("login")
+    public R login() {
+        return R.ok().data("token", "admin");
+    }
+
+    @GetMapping("info")
+    public R info() {
+        return R.ok().data("roles", "[admin]").data("name", "admin").data("avatar", "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+    }
+}
