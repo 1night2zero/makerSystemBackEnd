8d1c1ae3d9145978e6baa3b287046f5e4400b0c3
1night2zero <1night2zero@gmail.com>
2023-04-06 23:34:15 +0800

    完善了小节的CRUD接口

--- service/service_edu/src/main/java/zstu/edu/eduservice/controller/EduVideoController.java
+++ service/service_edu/src/main/java/zstu/edu/eduservice/controller/EduVideoController.java
@@ -1,9 +1,12 @@
 package zstu.edu.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import zstu.edu.commonutils.R;
+import zstu.edu.eduservice.entity.EduVideo;
+import zstu.edu.eduservice.service.EduVideoService;
 
 /**
  * <p>
@@ -15,7 +18,39 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/eduservice/edu-video")
+@CrossOrigin
 public class EduVideoController {
 
+    @Autowired
+    private EduVideoService eduVideoService;
+
+    // 添加小节
+    @PostMapping("addVideo")
+    public R addVideo(@RequestBody EduVideo eduVideo) {
+        eduVideoService.save(eduVideo);
+        return R.ok();
+    }
+
+    // 删除小节
+    // TODO 后面要删除小节里面的视频
+    @DeleteMapping("{id}")
+    public R deleteVideo(@PathVariable String id) {
+        eduVideoService.removeById(id);
+        return R.ok();
+    }
+
+    // 修改小节
+    @PostMapping("updateVideo")
+    public R updateVideo(@RequestBody EduVideo eduVideo) {
+        eduVideoService.updateById(eduVideo);
+        return R.ok();
+    }
+
+    // 根据id查询小节
+    @GetMapping("getVideoInfo/{videoId}")
+    public R getVideoInfo(@PathVariable String videoId) {
+        EduVideo eduVideo = eduVideoService.getById(videoId);
+        return R.ok().data("video", eduVideo);
+    }
 }
 
