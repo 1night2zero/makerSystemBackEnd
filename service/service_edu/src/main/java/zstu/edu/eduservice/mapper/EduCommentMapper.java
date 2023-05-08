6fdc391794536c922e0e462f062e84229fd79252
1night2zero <1night2zero@gmail.com>
2023-05-09 00:07:28 +0800

    用户课程评论功能

---
+++ service/service_edu/src/main/java/zstu/edu/eduservice/mapper/EduCommentMapper.java
@@ -0,0 +1,16 @@
+package zstu.edu.eduservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import zstu.edu.eduservice.entity.EduComment;
+
+/**
+ * <p>
+ * 评论 Mapper 接口
+ * </p>
+ *
+ * @author mier
+ * @since 2023-04-27
+ */
+public interface EduCommentMapper extends BaseMapper<EduComment> {
+
+}
